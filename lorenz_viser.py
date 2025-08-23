added better color gradient, fixed points referencing with copy() and added variables for MAX and MIN size
Signed-off-by: Iván López Broceño <ivanlpzbrc01@gmail.com>

lorenz_viser.py
@@ -29,19 +29,23 @@ def update_lorenz(points, time_step):
 def main():
     server = viser.ViserServer()
 
-    n_points = 10
+    n_points = 15
 
     # sample randomly position in 3D
     lorenz_points = np.random.rand(n_points, 3)
 
     n_trail = 30
-    point_sizes = np.linspace(0.1, 0.01, n_trail + 1)
-    lorenz_trail = [lorenz_points] * (n_trail + 1)
+    MAX_SIZE = 0.3
+    MIN_SIZE = 0.1
+    point_sizes = np.linspace(MAX_SIZE, MIN_SIZE, n_trail + 1)
+    lorenz_trail = [lorenz_points.copy()] * (n_trail + 1)
 
-    print(type(lorenz_trail))
-    # colors = np.zeros((n_points, 3), dtype=np.uint8)
+    red_color = np.full(n_trail+1, 255, dtype=np.uint8)
+    gradient_green = np.linspace(0, 255, n_trail+1, dtype=np.uint8)
+    blue_color = np.zeros(n_trail+1, dtype=np.uint8) 
 
- 
+    red2yellow_gradient = np.stack([red_color, gradient_green, blue_color], axis=1) 
+    print(red2yellow_gradient.shape)
     print("Visit: http://localhost:8080")
 
     time_step = 0.01
@@ -53,20 +57,26 @@ def main():
 
         lorenz_points = update_lorenz(lorenz_points, time_step)
         
+        # print difference between updated points and the last index
+
+        # diff = lorenz_points - lorenz_trail[0]
+        # print(diff)
         # shift points in lorenz_trail
         for i in range(len(lorenz_trail) - 2, -1, -1):
-            lorenz_trail[i+1] = lorenz_trail[i]
+            # print(i+1, i)
+            lorenz_trail[i+1] = lorenz_trail[i].copy()
 
-        lorenz_trail[0] = lorenz_points
+
+        lorenz_trail[0] = lorenz_points.copy()
 
 
         # display lorenz_trail
         for i in range(len(lorenz_trail)):
-
+            # print("lorenz_trail", i)
             server.scene.add_point_cloud(
                 name="/lorenz_cloud_" + str(i),
                 points=lorenz_trail[i],
-                colors=(0, 0, 0), 
+                colors=red2yellow_gradient[i, :], 
                 point_size=point_sizes[i],
                 point_shape = "rounded"
             )