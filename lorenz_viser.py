01ff4a4f29f6d42bbb94477a752b8d902b17dc57
Iván López Broceño <ivanlpzbrc01@gmail.com>
2025-08-24 12:34:58 +0200

    refactored with global constant variables
    Signed-off-by: Iván López Broceño <ivanlpzbrc01@gmail.com>

--- lorenz_viser.py
+++ lorenz_viser.py
@@ -4,10 +4,15 @@ import numpy as np
 import viser
 
 
-def update_lorenz(points, time_step):
-    sigma = 10.
-    ro = 28.
-    beta = 8./3.
+N_POINTS = 40
+N_TRAIL = 25
+
+MAX_SIZE = 0.3
+MIN_SIZE = 0.1
+
+TIME_STEP = 0.01
+
+def update_lorenz(points, dt, sigma = 10., ro = 28., beta = 8./3.):
     for i in range(len(points)):
 
         x = points[i, 0]
@@ -18,9 +23,9 @@ def update_lorenz(points, time_step):
         dy = x * (ro - z) - y
         dz = x * y - beta * z
 
-        x = x + time_step * dx
-        y = y + time_step * dy
-        z = z + time_step * dz
+        x = x + dt * dx
+        y = y + dt * dy
+        z = z + dt * dz
         points[i, 0] = x
         points[i, 1] = y
         points[i, 2] = z
@@ -29,32 +34,26 @@ def update_lorenz(points, time_step):
 def main():
     server = viser.ViserServer()
 
-    n_points = 15
-
     # sample randomly position in 3D
-    lorenz_points = np.random.rand(n_points, 3)
+    lorenz_points = np.random.rand(N_POINTS, 3)
 
-    n_trail = 30
-    MAX_SIZE = 0.3
-    MIN_SIZE = 0.1
-    point_sizes = np.linspace(MAX_SIZE, MIN_SIZE, n_trail + 1)
-    lorenz_trail = [lorenz_points.copy()] * (n_trail + 1)
+    
+    point_sizes = np.linspace(MAX_SIZE, MIN_SIZE, N_TRAIL + 1)
+    lorenz_trail = [lorenz_points.copy()] * (N_TRAIL + 1)
 
-    red_color = np.full(n_trail+1, 255, dtype=np.uint8)
-    gradient_green = np.linspace(0, 255, n_trail+1, dtype=np.uint8)
-    blue_color = np.zeros(n_trail+1, dtype=np.uint8) 
+    red_color = np.full(N_TRAIL+1, 255, dtype=np.uint8)
+    gradient_green = np.linspace(0, 255, N_TRAIL+1, dtype=np.uint8)
+    blue_color = np.zeros(N_TRAIL+1, dtype=np.uint8) 
 
     red2yellow_gradient = np.stack([red_color, gradient_green, blue_color], axis=1) 
 
     print("Visit: http://localhost:8080")
 
-    time_step = 0.01
-
     while True:
 
-        time.sleep(time_step)
+        time.sleep(TIME_STEP)
 
-        lorenz_points = update_lorenz(lorenz_points, time_step)
+        lorenz_points = update_lorenz(lorenz_points, TIME_STEP)
         
         # shift points in lorenz_trail, start from the back
         for i in range(len(lorenz_trail) - 2, -1, -1):
